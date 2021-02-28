c26ce37da2a4a8027ddc0de8ba020a88bf55b726
maywngml <maywngml@naver.com>
2021-02-28 18:48:25 +0900

    리팩토링: 메인페이지 소개글 추가

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import axios from 'axios';
 import '../styles/Home.scss';
 
 function Home() {
 	return (
 		<div className="home">
-			<b>메인페이지</b>
+			<div className="introduce">
+				<h3>녹음하고⏺️ 듣고👂 정리하고📖</h3>
+				<h3>이 귀찮은 걸 언제 다해?🔥</h3>
+				<h3>우리랑 같이 S-STT🎙️에서 하자!</h3>
+				<h3>녹음 파일을 쉽고 빠르게 텍스트로 만들어주는데</h3>
+				<h3>심지어 요약까지 해주는 웹 페이지</h3>
+			</div>
 		</div>
 	);
 }
